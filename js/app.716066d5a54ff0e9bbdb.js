9d302fa6d38e8247732a4de7b0ad1038f66cdf72
MarcinMac <jimrajnor@gmail.com>
2019-03-07 23:03:26 +0100

    repair small padding bug

--- js/app.716066d5a54ff0e9bbdb.js
+++ js/app.716066d5a54ff0e9bbdb.js
@@ -13894,7 +13894,7 @@ function(module) {
 
 // removed by extract-text-webpack-plugin
     if(false) {
-      // 1551995268410
+      // 1551995939918
       var cssReload = require("!../node_modules/css-hot-loader/hotModuleReplacement.js")(module.id, {"fileMap":"{fileName}"});
       module.hot.dispose(cssReload);
       module.hot.accept(undefined, cssReload);;
